04b5fd533092c9d6a4f552502b678ecadf001323
группы стационара <st@itstep.lan>
2021-11-08 20:09:41 +0300

    added new branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
+        System.out.println("Angels have more fun");
 	// write your code here
     }
 }
